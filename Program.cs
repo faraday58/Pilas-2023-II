27db939b3236a1d8331ea1c3542835778b52cb93
Armando Rodríguez <jarodriguezvera@gmail.com>
2023-05-03 12:48:15 -0600

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+namespace Pilas
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Stack<string> pila = new Stack<string>();
+
+            pila.Push("Hola");
+            pila.Push("Armando");
+
+            for(int i= 0; i< pila.Count; i++  )
+            {
+                Console.Write(" {0} ", pila.Pop());
+            }
+            Console.ReadLine();
+        }
+    }
+}
